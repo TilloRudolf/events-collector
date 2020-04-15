ce91a819aa07cf13b8d6e9d244ea8dc03cecaf6c
tillorudolf <tillorudolf@gmail.com>
2020-04-15 14:36:50 +0300

    removed RequestBody

--- src/main/java/com/back/eventscollector/controller/EventsController.java
+++ src/main/java/com/back/eventscollector/controller/EventsController.java
@@ -4,12 +4,14 @@ import com.back.eventscollector.model.Event;
 import com.back.eventscollector.model.EventsResponse;
 import com.back.eventscollector.repository.EventsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class EventsController {
 
     @Autowired
@@ -20,28 +22,24 @@ public class EventsController {
     }
 
     @RequestMapping(value = "/minute", method = RequestMethod.GET)
-    @ResponseBody
     public EventsResponse eventsForLastMinute() {
         List<Event> events = repository.forMinute();
         return new EventsResponse(events);
     }
 
     @RequestMapping(value = "/hour", method = RequestMethod.GET)
-    @ResponseBody
     public EventsResponse eventsForLastHour() {
         List<Event> events = repository.forHour();
         return new EventsResponse(events);
     }
 
     @RequestMapping(value = "/day", method = RequestMethod.GET)
-    @ResponseBody
     public EventsResponse eventsForLastDay() {
         List<Event> events = repository.forDay();
         return new EventsResponse(events);
     }
 
     @RequestMapping(value = "/event", method = RequestMethod.POST)
-    @ResponseBody
     public EventsControllerResponse event(@RequestBody Event event) {
         repository.putEvent(event);
         return new EventsControllerResponse("SUCCESS", event.getName());
