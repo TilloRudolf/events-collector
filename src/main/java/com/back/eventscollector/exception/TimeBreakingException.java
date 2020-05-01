fa0788ee2290957174ec66eb4ef808f59af21b6a
tillorudolf <tillorudolf@gmail.com>
2020-05-01 17:51:58 +0300

    fixed exception message

--- src/main/java/com/back/eventscollector/exception/TimeBreakingException.java
+++ src/main/java/com/back/eventscollector/exception/TimeBreakingException.java
@@ -2,11 +2,11 @@ package com.back.eventscollector.exception;
 
 import com.back.eventscollector.model.Event;
 
-import java.util.Date;
+import java.time.Instant;
 
 public class TimeBreakingException extends RuntimeException {
     public TimeBreakingException(Event event) {
-        super(String.format("time of event: %s is in the future. You should wait some time",
-                new Date(event.getTimestampMillisUTC())));
+        super(String.format("time of event: %s (UTC) is in the future. You should wait some time",
+                Instant.ofEpochMilli(event.getTimestampMillisUTC())));
     }
 }
